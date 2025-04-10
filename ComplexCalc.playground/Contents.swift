05081165468cc81ea5356535e55c00d7f40c5ab8
arvinhakakian <arvinh16@uw.edu>
2025-04-09 21:10:08 -0700

    finished assignment

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -28,6 +28,75 @@ print("Welcome back to the UW Calculator")
 //: IMPORTANT: If any tests are commented out, you will be graded a zero (0)! You should never be in the habit of eliminating tests to make the code pass.
 //:
 class Calculator {
+    
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func add(_ args: [Int]) -> Int {
+        return args.reduce(0, +)
+    }
+    
+    func multiply(_ args: [Int]) -> Int {
+        return args.reduce(1, *)
+    }
+    
+    func count(_ args: [Int]) -> Int {
+        return args.count
+    }
+        
+    func avg(_ args: [Int]) -> Int {
+        if args.isEmpty { return 0 }
+        return add(args) / args.count
+    }
+
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        return op(lhs, rhs)
+    }
+
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        return args.reduce(beg, op)
+    }
+
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        var result = [String: Int]()
+        for key in lhs.keys {
+            if let l = lhs[key], let r = rhs[key] {
+                result[key] = l + r
+            }
+        }
+        return result
+    }
+
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        var result = [String: Int]()
+        for key in lhs.keys {
+            if let l = lhs[key], let r = rhs[key] {
+                result[key] = l - r
+            }
+        }
+        return result
+    }
 }
 
 //: Don't change the name of this object (`calc`); it's used in all the tests.
@@ -52,7 +121,7 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
